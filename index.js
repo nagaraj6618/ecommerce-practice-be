1752a3fb13fa9a386ac7d4ed1de7a6cea0fde1ee
NAGARAJ S <nagaraj516700@gmail.com>
2024-07-18 13:40:32 +0530

    Feat : Basic sever Created

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+require('dotenv').config();
 const cors = require('cors');
+const PORT = process.env.PORT;
+const DB_URL = process.env.MONGODB_URL;
+
+const app = express();
+
+
+(async function dbConnection() {
+   try{
+      const DBResponse = await mongoose.connect(DB_URL);
+      console.log("DB Connected");
+   }
+   catch(error){
+
+      console.log(error);
+   }
+})();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors({origin:true,credentials:true}));
+
+
+app.get('/api/v1/',(req,res) => {
+   res.status(200).json({
+
+      message:"Server Working",
+      success:true,
+   });
+   
+})
+
+
+app.listen(PORT,()=> console.log("Server running.."))
+
 
