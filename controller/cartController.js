374775d6fac3675f9451c677cc8e63b8ebb6a474
NAGARAJ S <nagaraj516700@gmail.com>
2024-08-01 10:09:50 +0530

    Bug : send product details along with cartData

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,30 +2,38 @@ const { verifyToken } = require("../middleware/authMiddleware");
 const CartModel = require("../model/CartModel");
 const productModel = require("../model/productModel");
 
-async function getAllCart(req,res) {
-   try{
+async function getAllCart(req, res) {
+   try {
       const user = verifyToken(req.headers.authorization);
-      if(!user){
+      if (!user) {
          return res.status(401).json({
-            message:"Please login..",
-            success:false,
+            message: "Please login..",
+            success: false,
          });
       }
 
-      const cartProduct = await CartModel.find({userId:user.id});
+      const cartProduct = await CartModel.find({ userId: user.id });
+      const productDatas = await Promise.all(cartProduct.map(async (cartData) => {
+         const product = await productModel.findById(cartData.productId);
+         const singleCartData = { cartData, productData:product };
+         return singleCartData;
+      }));
+
+     
+
 
       res.status(200).json({
-         message:"Retrived all the products in the cart..",
-         success:true,
-         data:cartProduct
+         message: "Retrived all the products in the cart..",
+         success: true,
+         data:  productDatas
       });
 
    }
-   catch(error){
+   catch (error) {
       res.status(500).json({
-         message:error.message,
-         success:false
-         }
+         message: error.message,
+         success: false
+      }
       );
    }
 };
@@ -33,93 +41,94 @@ async function getAllCart(req,res) {
 
 
 
-async function addProductToCart(req,res) {
-   try{
-      // const {}
+async function addProductToCart(req, res) {
+   try {
+      
       const user = verifyToken(req.headers.authorization);
-      const {productId} = req.body.product;
+      const { productId } = req.body.product;
 
-      if(!user){
+      if (!user) {
          return res.status(401).json({
-            message:'Please Login..',
-            success:false,
+            message: 'Please Login..',
+            success: false,
          })
       }
 
       const isProductExist = await productModel.find(
-         {_id:productId}
+         { _id: productId }
       );
 
-      if(isProductExist.length<=0){
+      if (isProductExist.length <= 0) {
          return res.status(404).json({
-            message:"Product not found.",
-            status:false
+            message: "Product not found.",
+            status: false
          });
       };
 
 
       const isAlreadyExistProduct = await CartModel.find({
-         userId:user.id,productId:productId
+         userId: user.id, productId: productId
       })
       // console.log(isAlreadyExistProduct)
-      if(isAlreadyExistProduct.length>0){
+      if (isAlreadyExistProduct.length > 0) {
 
          const existProduct = isAlreadyExistProduct[0]
          const existProductId = existProduct._id;
          // console.log(existProductId);
-         await CartModel.findByIdAndUpdate(existProductId,{
-            count:existProduct.count+1
+         await CartModel.findByIdAndUpdate(existProductId, {
+            count: existProduct.count + 1
          })
-         
+
          return res.status(200).json({
-            message:"Product count updated",
-            success:true
+            message: "Product count updated",
+            success: true
          });
       }
 
-      
-      console.log(isProductExist);
+
+      // console.log(isProductExist);
 
 
 
       const newCartProduct = await CartModel({
-         userId:user.id,
-         productId:productId,
+         userId: user.id,
+         productId: productId,
       })
 
       // console.log(newCartProduct)
       await newCartProduct.save();
       res.status(201).json({
-         message:"Added product to the cart..",
-         success:true,
+         message: "Added product to the cart..",
+         success: true,
       });
 
    }
-   catch(error){
+   catch (error) {
       res.status(500).json({
-         message:error.message,
-         success:false
+         message: error.message,
+         success: false
       })
    }
 }
 
-async function getCartProductById (req,res) {
-   try{
-      const {id} = req.params;
-      const product = await CartModel.findById(id);
+async function getCartProductById(req, res) {
+   try {
+      const { id } = req.params;
+      const cartProduct = await CartModel.findById(id);
+      const product = await productModel.findById(cartProduct.productId);
       res.status(200).json({
-         message:"Retrived product data by ID",
-         success:true,
-         data:product
+         message: "Retrived product data by ID",
+         success: true,
+         data: {cartData:cartProduct,productData:product}
       });
 
    }
-   catch(error){
+   catch (error) {
       res.status(500).json({
-         message:error.message,
-         success:false,
+         message: error.message,
+         success: false,
       })
    }
 }
 
-module.exports = {getAllCart,addProductToCart,getCartProductById};
+module.exports = { getAllCart, addProductToCart, getCartProductById };
